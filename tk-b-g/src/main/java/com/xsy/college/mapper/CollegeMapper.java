2a43c3051b97137ca8d4adbea369aff869042b34
name <eamil-address>
2020-07-08 11:20:09 +0800

    初始化通用mapper

---
+++ tk-b-g/src/main/java/com/xsy/college/mapper/CollegeMapper.java
@@ -0,0 +1,7 @@
+package com.xsy.college.mapper;
+
+import com.xsy.college.pojo.College;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CollegeMapper extends Mapper<College> {
+}
---
+++ tk-b-g/src/main/java/com/xsy/college/mapper/MajorMapper.java
@@ -0,0 +1,7 @@
+package com.xsy.college.mapper;
+
+import com.xsy.college.pojo.Major;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MajorMapper extends Mapper<Major> {
+}
